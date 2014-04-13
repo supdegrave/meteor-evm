890ec70c2e57fe41376ffae4124272fee64609ea
Stuart Updegrave <stuart@updegrave.com>
2014-04-13 18:08:36 +0200

    Teams: display selected users when in read mode

--- client/views/teams/teams.js
+++ client/views/teams/teams.js
@@ -1,13 +1,17 @@
+function displayName(user) {
+  if (!user) { user = this; }
+  
+  return user.username 
+          || (user.profile && user.profile.name) 
+            || (user.emails && user.emails[0].address);
+}
+
 UI.registerHelper('canEdit', function() {
   return Roles.userIsInRole(Meteor.userId(), 'admin') 
     || _.contains([this.owner, this.lead, this.colead], Meteor.userId());
 });
 
-UI.registerHelper('displayName', function() {
-  return this.username 
-          || (this.profile && this.profile.name) 
-            || (this.emails && this.emails[0].address);
-});
+UI.registerHelper('displayName', displayName);
 
 Template.team.helpers({
   getParent: function(parentId) {
@@ -19,6 +23,13 @@ Template.role.helpers({
   users: function() {
     return Meteor.users.find();
   },
+  selectedUser: function() {
+    if (this.team)
+    {
+      var user = Meteor.users.findOne({_id: this.team[this.role]});
+      return displayName(user) || "none";
+    } 
+  }
 });
 
 Template.role.events({
@@ -28,4 +39,3 @@ Template.role.events({
     }
   }
 });
-
