7335480088f848f7b8abfc5c88e1c75657781840
Stuart Updegrave <stuart@updegrave.com>
2014-04-13 17:38:12 +0200

    DRY up team roles template

--- client/views/teams/teams.js
+++ client/views/teams/teams.js
@@ -1,28 +1,31 @@
+UI.registerHelper('canEdit', function() {
+  return Roles.userIsInRole(Meteor.userId(), 'admin') 
+    || _.contains([this.owner, this.lead, this.colead], Meteor.userId());
+});
+
+UI.registerHelper('displayName', function() {
+  return this.username 
+          || (this.profile && this.profile.name) 
+            || (this.emails && this.emails[0].address);
+});
+
 Template.team.helpers({
   getParent: function(parentId) {
     return Teams.findOne({id: parentId});
   },
 });
 
-UI.registerHelper('canEdit', function() {
-  return Roles.userIsInRole(Meteor.userId(), 'admin') 
-    || _.contains([this.owner, this.lead, this.colead], Meteor.userId());
+Template.role.helpers({
+  users: function() {
+    return Meteor.users.find();
+  },
 });
 
-UI.registerHelper('users', function(role) {
-  return Meteor.users.find().fetch().map(function(user) {
-    if (user._id === this[role]) { 
-      user["is_" + role] = true;
+Template.role.events({
+  'DOMNodeInserted select': function(evt) {
+    if (this.team) {
+      $(evt.target).val(this.team[this.role]);
     }
-    return user;
-  }, this);
+  }
 });
 
-UI.registerHelper('displayName', function() {
-  // var user = Meteor.users.findOne({_id: userId}) || this;
-  // if (userId) { console.log(Meteor.users.findOne({_id: userId})); }
-  // else { console.log(this); }
-  
-  console.log(this);
-  return (this.thisname || this.profile && this.profile.name) || this.emails[0].address;
-});
