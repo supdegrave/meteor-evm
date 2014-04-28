8fa906c6dfd7a979df2b89e72752d1961a4bac1d
MonkeyDo <salicon.petiller@gmail.com>
2014-04-28 14:08:41 +0200

    UI changes. Added user profile and user list pages

---
+++ client/views/users/list_users.js
@@ -0,0 +1,24 @@
+Template.listUsers.helpers({
+  users: function() {
+    return filteredUserQuery(Meteor.userId(), Session.get("userFilter"));
+  },
+  myself: function(userId) {
+    return Meteor.userId() === userId;
+  },
+  isOrganizer: function() {
+    return this.organizer;
+  }, 
+  memberOfTeams: function(userId) {
+    return Teams.find({members:{$in: [this._id]}},{name:1});
+  }
+});
+
+Template.listUsers.events({
+  'click .glyphicon-info-sign': function(event, template) {
+    Session.set('userInScope', this);
+  }   
+});
+
+Template.listUsers.rendered = function() {
+   
+};
