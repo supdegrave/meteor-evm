0b6be06ee3a1e5114bc0b186fabf1f17a2c49104
MonkeyDo <salicon.petiller@gmail.com>
2014-06-09 17:41:54 +0200

    Reworked layout and switched to Semantic UI

--- client/views/org/organizers.js
+++ client/views/org/organizers.js
@@ -3,4 +3,13 @@ Template.org.helpers({
     return Meteor.userId() === this._id 
             || _.contains(Meteor.user().roles, 'admin');
   }
-})
+});
+
+Template.orgTreeItem.helpers({
+  teamChildren: function(){
+    return Teams.find({parentId: this._id});
+  }
+});
+Template.orgTreeItem.rendered = function(){
+  $('.ui.accordion').accordion("exclusive",false);
+};
