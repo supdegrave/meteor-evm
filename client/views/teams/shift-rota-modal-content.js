d01a494a63512a1bc3a7d279fcc3eb6d5c9c6ae6
Stuart Updegrave <stuart@updegrave.com>
2015-01-27 20:51:38 +0100

    shift/rota: hide event/rota popup on create

--- client/views/teams/shift-rota-modal-content.js
+++ client/views/teams/shift-rota-modal-content.js
@@ -201,7 +201,7 @@ Template.shiftRotaModalContent.rendered = function() {
         }
         else {
           createShiftOrRota();
-          return false;
+          return true;
         }
       }
     });
