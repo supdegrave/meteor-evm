4a14fc299c269ce5966b9dcb44f1930b97d9fb8b
Stuart Updegrave <stuart@updegrave.com>
2015-02-02 16:29:55 +0100

    teams / open requests: user display name is now a link to profile modal

--- client/views/teams/manageRequests.js
+++ client/views/teams/manageRequests.js
@@ -1,4 +1,5 @@
 Template.manageRequests.events({
+  // approve volunteer request to join a shift
   'click a > i.checkmark.icon': function(evt, tmpl) { 
     var eventId = tmpl.data._id, 
         userId  = evt && evt.currentTarget.dataset && evt.currentTarget.dataset.userid;
@@ -8,12 +9,24 @@ Template.manageRequests.events({
     }
   },
 
+  // deny volunteer request to join a shift
   'click a > i.remove.icon': function(evt, tmpl) { 
     var eventId = tmpl.data._id, 
         userId  = evt && evt.currentTarget.dataset && evt.currentTarget.dataset.userid;
         
     if (eventId && userId) {
       Meteor.call('denyVolunteerRequest', eventId, userId);
+    }
+  },
+  
+  // show user profile modal dialog
+  'click .modalCallButton': function(evt, tmpl) {
+    var userId      = evt && evt.currentTarget.dataset && evt.currentTarget.dataset.userid,
+        modalTarget = $(evt.currentTarget).data("target");
+        
+    if (userId && modalTarget) {
+      Session.set('userInScope', Meteor.users.findOne(userId));
+      $(modalTarget).modal('show');
     }
   }
 });
