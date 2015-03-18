b9640a1612c1f46f52adc55c35a8591e60a7ac8e
Monkey <salicon.petiller@gmail.com>
2015-03-18 10:42:10 +0100

    Added 'delete' functionnality for user skills

--- client/views/user/user_profile.js
+++ client/views/user/user_profile.js
@@ -6,6 +6,11 @@ Template.userProfile.events({
       var modalTarget = $(event.currentTarget).data("target");
       $(modalTarget).modal('show');
     },
+  'click .deleteSkill': function(event, template) {
+      console.log("need to delete this: "+this);
+      console.log("need to delete this: "+this.name);
+      Meteor.users.update(Meteor.userId(), { $pull: { "profile.skills": this }});
+    },
 });
 
 Template.userProfile.rendered = function() {
